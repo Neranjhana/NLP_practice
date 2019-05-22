72490cf6b8d367aab1db2d59589d50c0d0551d36
Neranjhana <neranjhanaramesh@gmail.com>
2019-05-22 17:01:09 +0530

    Writing to excel

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,9 +9,12 @@ Created on Wed May 22 14:41:06 2019
 import sys
 import csv
 import collections
+import xlsxwriter
+workbook=xlsxwriter.Workbook('/home/spiglobal/rec.xlsx')
+worksheet = workbook.add_worksheet()
 csv.field_size_limit(sys.maxsize)
 from numpy import array
-f=open("/home/spiglobal/ACTA_NEUROLOGICA_SCANDINAVICA.txt")
+f=open("/home/spiglobal/AMERICAN JOURNAL OF MEDICAL GENETICS PART B.txt")
 file_reader= csv.reader(f, delimiter='|')
 article_id=[]
 journal_title=[]
@@ -21,6 +24,7 @@ article_body=[]
 records=[]
 for line in file_reader:
     article_id.append(line[0])
+    journal_title.append(line[1])
     journal_doi.append(line[2])
     article_title.append(line[3])
     article_body.append(line[4])
@@ -32,7 +36,7 @@ print(len(article_title[0].split()))
 count_removed_records=0
 count_pr=0
 for line in records:
-    if(len(line[4].split())<5):
+    if(len(line[4].split())>20000):
         count_removed_records=count_removed_records+1
         removed_records.append(line)
     else:
@@ -49,6 +53,16 @@ for line in perfect_records:
     word_count+=len(line[4].split())
 print("Number of empty records is",count_removed_records-1)
 print("Number of proper records is",count_pr-1)
+
 print(word_count)
 print(count_pr)
 print("The average article length is",word_count/count_pr)
+row=0
+for column,data in enumerate(records):
+    worksheet.write_column(row,column,data)
+workbook.close()
+
+
+
+
+
